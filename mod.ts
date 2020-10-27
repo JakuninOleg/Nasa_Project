cda25799d5a26c198abeddf875c56d77ece6d920
Oleg Jakunin <oleg.kemz@gmail.com>
2020-10-27 22:59:21 +0300

    added router to the app

--- mod.ts
+++ mod.ts
@@ -1,15 +1,26 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, send } from "https://deno.land/x/oak/mod.ts";
+import api from './api.ts';
 
 const app = new Application();
 const PORT = 8000;
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World!";
+app.use(api.routes());
+
+app.use(async (ctx) => {
+  const filePath = ctx.request.url.pathname;
+  const fileWhitelist = [
+    "/index.html",
+    "/javascripts/script.js",
+    "/stylesheets/style.css",
+    "/images/favicon.png",
+  ];
+  if (fileWhitelist.includes(filePath)) {
+    await send(ctx, filePath, {
+      root: `${Deno.cwd()}/public`,
+    });
+  }
 });
 
-app.use(async (ctx, next) => {
-  await next();
-})
 
 if (import.meta.main) {
   app.listen({
